6a3d62ab1c1db64745f3682b8b9648ac31f030d5
Henry Kivelä <henry.kivela@lapinamk.fi>
2021-11-07 10:07:05 +0200

    Update run_atv_robot_remote1.sh

--- atv_remote/Home/run_atv_robot_remote1.sh
+++ atv_remote/Home/run_atv_robot_remote1.sh
@@ -4,11 +4,11 @@
 source ip_addresses.sh
 echo "welcome controller"
 # need install sshpass
-#sshpass -p $hoppop ssh -o StrictHostKeyChecking=no $my_username@$robot_IP
+#sshpass -p $my_pass_word ssh -o StrictHostKeyChecking=no $my_username@$robot_IP
 #sleep 5s #wait
 gnome-terminal -- /bin/sh -c 'bash run_atv_robot_remote2.sh'&
 gnome-terminal -- /bin/sh -c 'bash run_atv_robot_remote3.sh'&
-sshpass -p $hoppop ssh -tty $my_username@$robot_IP << endoflist
+sshpass -p $my_pass_word ssh -tty $my_username@$robot_IP << endoflist
 source ~/.bashrc
 export ROS_MASTER_URI=http://$robot_IP:11311
 export ROS_IP=$robot_IP
