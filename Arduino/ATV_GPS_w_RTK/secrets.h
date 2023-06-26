8071e1f8484289fb682bf2c2fec44a8924a9bee8
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-06-26 13:55:43 +0300

    Update secrets.h

--- Arduino/ATV_GPS_w_RTK/secrets.h
+++ Arduino/ATV_GPS_w_RTK/secrets.h
@@ -8,3 +8,6 @@ const uint16_t casterPort = 2101;
 const char casterUser[] = ""; //ADD HERE YOUR EMAIL
 const char casterUserPW[] = "";
 const char mountPoint[] = "Taroniemi"; //The mount point you want to get data from
+
+// To get correction data from the NLS, go to their website:  https://www.maanmittauslaitos.fi/en/finpos/rtk
+// Register and send a request for the free RTK stream for research and education
