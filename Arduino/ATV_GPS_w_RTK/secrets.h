ab66caf199b3cdb2654adfa222eaad3ca69cd959
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-06-26 13:51:53 +0300

    Create secrets.h
    Add your Wifi credentials and your RTK source credentials

---
+++ Arduino/ATV_GPS_w_RTK/secrets.h
@@ -0,0 +1,10 @@
+//ADD HERE THE WIFI NAME AND PASSWORD with witch you want to receive the GPS correction stream
+const char ssid[] = "";
+const char password[] =  "";
+
+//RTK2Go works well and is free
+const char casterHost[] = "rtk2go.com"; 
+const uint16_t casterPort = 2101;
+const char casterUser[] = ""; //ADD HERE YOUR EMAIL
+const char casterUserPW[] = "";
+const char mountPoint[] = "Taroniemi"; //The mount point you want to get data from
