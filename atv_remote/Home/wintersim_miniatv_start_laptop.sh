327a43c0ec5ae3a9bee0ceddbfb9801c60a1a9df
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-08-03 19:29:18 +0300

    Create wintersim_miniatv_start_laptop.sh
    Starts every necessary program in the laptop

---
+++ atv_remote/Home/wintersim_miniatv_start_laptop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sleep 5s
+# Start everything on the Laptop/PC side:
+# First, make this file executable!
+# source /opt/carla-ros-bridge/melodic/setup.bash has to be added to the bash.rc beforehand!
+
+# Starts in Laptop:
+#	- WinterSIM
+#	- carla_ros_bridge (and with that a roscore)
+#	- carla_ackermann_control
+#	- multimaster_fkie (first discover the miniATV roscore, then sync)
+
+
+gnome-terminal --tab -e "WinterSim1.2_rantavitikka/CarlaUE4.sh" 
+#gnome-terminal --tab -e ""
+gnome-terminal --tab -e "roslaunch carla_ros_bridge carla_ros_bridge_with_example_ego_vehicle.launch"
+gnome-terminal --tab -e "roslaunch carla_ackermann_control carla_ackermann_control.launch"
+# multimaster_fkie communication between two seperate rosmasters
+# You have to setup the multimaster according to my Multimaster_fkie Documentation.odt or add the setup commands here!
+sleep 6s
+
+export ROS_MASTER_URI=http://cs-worklaptop:11311
+
+sleep 2s
+gnome-terminal --tab -e "node_manager"
+gnome-terminal --tab -e "rosrun master_discovery_fkie master_discovery _robot_hosts:=[ATV]"
+sleep 6s
+gnome-terminal --tab -e "rosrun master_sync_fkie master_sync"
