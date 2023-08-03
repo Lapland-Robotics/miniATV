99f29062d0ea366b5acfe1565afbb60dc6647441
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-08-03 19:40:05 +0300

    Update ip_addresses.sh
    updated variable names for wintersim_miniatv_start.sh

--- atv_remote/Home/ip_addresses.sh
+++ atv_remote/Home/ip_addresses.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-#Addresses for robot launch
+
+# Comment out what you don't need! Fill in the passwords!
+
+#Addresses for robot launch (for the older files)
 #readonly robot_IP=192.168.0.13
 readonly robot_IP=172.16.200.200
 #readonly this_remote_PC_IP=192.168.0.12
 readonly this_remote_PC_IP=172.16.200.199
-
 readonly my_username=hk #< YOUR USERNAME >
 readonly my_pass_word=XXXXXX #< YOUR USERNAME >
+
+# For the wintersim_miniatv_start.sh 
+readonly this_remote_PC_IP= cs-worklaptop.local # or your machine's name
+readonly robot_IP=ATV.local
+readonly atv_username=hk
+readonly atv_password= <miniATV password>
