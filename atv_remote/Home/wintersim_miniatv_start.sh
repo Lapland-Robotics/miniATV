98c2baf763928b0505457a3b87312cc1f1abae2c
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-08-03 19:26:49 +0300

    Create wintersim_miniatv_start.sh
    Starts everything in the Laptop and miniATV with only one command

---
+++ atv_remote/Home/wintersim_miniatv_start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# first, make this file executable!
+# run this file:  bash wintersim_miniatv_start.sh 
+
+gnome-terminal --window -e "bash 'wintersim_miniatv_start_laptop.sh'"
+
+gnome-terminal --window -e "bash 'wintersim_miniatv_start_miniatv.sh'"
+
+# needed: ip_adresses.sh with all the credentials for starting the miniATV
+
+# Starts in Laptop:
+#	- WinterSIM
+#	- carla_ros_bridge (and with that a roscore with use_sim_time = True)
+#	- carla_ackermann_control
+#	- multimaster_fkie (first discover the miniATV roscore, then sync)
+
+# Starts in miniATV:
+#	- roscore (with use_sim_time = False)
+#	- multimaster_fkie (first discover the Laptop roscore, then sync)
+#	- launch the desired ros launchfile 
