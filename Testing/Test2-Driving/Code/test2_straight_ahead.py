14cc8e3c84ec5efe7936345429deba0e41d454a8
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-09-08 15:30:06 +0300

    Update test2_straight_ahead.py
    Test script works and is almost ready

--- Testing/Test2-Driving/Code/test2_straight_ahead.py
+++ Testing/Test2-Driving/Code/test2_straight_ahead.py
@@ -1,129 +1,121 @@
 #!/usr/bin/env python
 
-# By C. Stubenvoll
 
 """
-Script for 
-- comparing the current behaviour of the physical miniATv and it's digital twin in WinterSIM 
-- finding delays in the current setup
+By C. Stubenvoll
 
-And using the data aquired through this script as a benchmark
+Script for a test drive with the physical miniATV and it's digital twin in WinterSIM:
+It starts the recording of a rosbag (recording all topics of the physical and digital miniATV) into a designated folder,
+then waits one minute so that the physical miniATV's GNSS can get a more precise measurement of the start point (speed command = 0 m/s),
+after that sends a command to both miniATV's steering topics to drive with maximum speed 
+(maximum speed is definde in both miniATVs parameters, the steering topic gives a higher number that will be clipped to that individual maximum),
+after 10s of driving (roughly 10m for the physical miniATV), a speed = 0 command will be sent to both miniATVs 
+and another one minute wait time for the GNSS starts.
+Since speed = 0 only switches off the motors and doesn't activate any breaks, both miniATVs will roll while losing speed until they stand still,
+this is included in the wait time and will later be filtered out for the GNSS end position with the miniATV's status topics 
+that show when the actual miniATV's speed is down to 0 again.
+See the test procedure document for more information on the test setup.
+
+The collected data is expected to give insight 
+- on the similarity in movement of the physical miniATv and it's digital twin in WinterSIM 
+- on delays in the current setup (f.ex. due to forwarding a topic)
+- on deviations from the straight line due to wear and tear of 3D printed parts in the steering mechanism of the physical miniATV
+- on the relationship between battery charge level and speed and acceleration of the physical miniATV
+
+And the data can also be used as a benchmark
 - for comparing future miniATV models in WinterSIM or in reality with it's previous versions
 - comparing new miniATV (models) to their counterpart in WinterSIM/realiy 
 - comparing the influence of different weather conditions
 
-
-1st test:
-
-Driving straight ahead on an even surface in the Rantavitikka testing ground (/parking lot) 
-through sending a max speed command for XXXXX seconds to the ackermann topic both miniATV's listen to 
 """
 
-import datetime
-import math
-import numpy
 import os
 import subprocess
-
-import carla
-
-import record_rosbag # TODO: Works???
+import time
 
 import rospy
-import rostopic
-import tf
-from std_msgs.msg import Bool
-from sensor_msgs.msg import NavSatFix
-from sensor_msgs.msg import Image
-from std_msgs.msg import String
-from carla_msgs.msg import CarlaCollisionEvent
-from carla_msgs.msg import CarlaLaneInvasionEvent
-from carla_msgs.msg import CarlaEgoVehicleControl
-from carla_msgs.msg import CarlaEgoVehicleStatus
-from carla_msgs.msg import CarlaEgoVehicleInfo
-from carla_msgs.msg import CarlaStatus
 from ackermann_msgs.msg import AckermannDriveStamped
 
 
-#important carla topics: 
+rospy.init_node('test2')
 
-# TODO: Don't use wait or delay for 10min, because the ros topic maybe needs contact?
-
-# TODO: Record rosbag 10s bevor and 10s after movement stopped 
-
-
-max_speed = 100 # There will be clipping according to the config parameters of each miniATV
-stop_speed = 0
-steering_angle = 0 # Straight ahead
+max_speed = 100 # [m/s] There will be clipping according to the config parameters of each miniATV, so this can be regarded as MAX speed
+stop_speed = 0 # [m/s]
+steering_angle = 0 # [deg] Straight ahead
 
 drive_msg = AckermannDriveStamped()
-test_cmd_to_pt_miniATV = rospy.Publisher('/emergency_cmd', AckermannDriveStamped, queue_size=1)
+test_cmd_to_pt_miniATV = rospy.Publisher('/ackermann_cmd', AckermannDriveStamped, queue_size=1)
 test_cmd_to_dt_miniATV = rospy.Publisher('/carla/ego_vehicle/ackermann_cmd', AckermannDriveStamped, queue_size=1)
 
-start_drive_time = rospy.get_time()
-    
 
 def publish_max_speed_cmd():
+    rospy.loginfo("speed = MAX")
     drive_msg.drive.speed = max_speed
     drive_msg.drive.steering_angle = steering_angle
-    drive_msg.header.stamp = rospy.Time.now()
+    #drive_msg.header.stamp = rospy.Time.now() # Does not work with WinterSIM/carla_ros_bridge simulation time
     test_cmd_to_dt_miniATV.publish(drive_msg)
     test_cmd_to_pt_miniATV.publish(drive_msg)
+
 
 def publish_stop_cmd():
+    rospy.loginfo("speed = 0")
     drive_msg.drive.speed = stop_speed
     drive_msg.drive.steering_angle = steering_angle
-    drive_msg.header.stamp = rospy.Time.now()
+    #drive_msg.header.stamp = rospy.Time.now() # Does not work with WinterSIM/carla_ros_bridge simulation time
     test_cmd_to_dt_miniATV.publish(drive_msg)
     test_cmd_to_pt_miniATV.publish(drive_msg)
 
-    
 
-# Go to class functions that do all the heavy lifting. Do error checking.
-drive_start = 0 # fill with rostime
-drive_time = 0
-wait_after_drive = 0
-# try:
-#     test_start = rospy.get_time()
-#     rosbag_record = record_rosbag()
-# except rospy.ROSInterruptException:
-#     rospy.loginfo("Test 2: Record Rosbag failed!!!")
-#     pass
-test_start = rospy.get_time()
-subprocess.Popen()
+drive_time = time.time()
+wait_after_drive = time.time()
 
-gnss_wait = rospy.Duration.from_sec(60)
-drive_duration = rospy.Duration.from_sec(10)
+owd = os.getcwd()
+print("Orig dir: " +  str(owd))
+test_start = time.time()
+print("test_start type: " + str(type(test_start)))
+os.chdir('/home/cs/Documents/Master_Thesis/Test_Data/Test2_Drive_Straight_Ahead')
+print("Changed dir: " + str(os.getcwd()))
+
+bag_record = subprocess.Popen(["/bin/bash", "./test2_start_rosbag_recording.sh"])
+
+rospy.loginfo("After bash script time: ")
+bash_dur = time.time() - test_start
+os.chdir(owd)
+rospy.loginfo(bash_dur)
+gnss_wait = 60 # [s] Waittime before and after drive to get a good average for the GNSS position of the start- and endpoint
+drive_duration = 10 # [s] Drive for 10 s, which equates roughly 10 m
+print("After change back dir: " + str(os.getcwd()))
     
 r = rospy.Rate(5)
-while not rospy.is_shutdown(): # Loop so contact with ROS stays open during wait times #TODO: Do I need to keep contact even?
-    if rospy.get_time() - test_start <= gnss_wait:
-        rospy.loginfo("Start wait before test drive!")
+# Can't use rospy.Time.now() instead of time.time(), or the miniATV will spawn at the wrong position (?!) 
+# and the while loop will jump directly to the end - it works without WinterSIM in the laptop, it works in the miniATV - 
+# so maybe it's carla's simulation time?
+while not rospy.is_shutdown():
+    # Before the test drive
+    if time.time() - test_start <= 10:
+        print("Wait before test drive!")
+        print(str(time.time() - test_start) + " of 10 s")
         # wait X min at starting position before the drive for the GNSS to have a precise positon
-        r.sleep()
-    elif drive_time <= drive_duration:
-        rospy.loginfo("Start test drive!")
-        #drive_start = rospy.get_time()
+        publish_stop_cmd()
+        drive_time = time.time()
+    # During the test drive
+    elif time.time() - drive_time <= 10:
+        print("Drive!")
+        print(str(time.time() - drive_time) + " of 10 s")
         publish_max_speed_cmd() # Max speed cmd to ackermann topics!
-        drive_time = rospy.get_time()
-    elif drive_time > drive_duration:
-        rospy.loginfo("Stop and wait for test end")
+        wait_after_drive = time.time()
+    # After the test drive
+    elif time.time() - drive_time > 10:
+        print("Wait for test end")
         # wait again X minutes for the miniATV to stop rolling and to have a precise GNSS positon of the end position
-        publish_stop_cmd
-        wait_after_drive = rospy.get_time()
-        if wait_after_drive <= gnss_wait:
-            publish_stop_cmd
-            r.sleep
+        if time.time() - wait_after_drive <= gnss_wait:
+            print("Wait after drive!")
+            print(str(time.time() - wait_after_drive) + " of 10 s")
+            publish_stop_cmd()
         else:
-            # try:
-            #     rosbag_record = rosbag_record.stop_recording_handler()
-            # except rospy.ROSInterruptException:
-            #     rospy.loginfo("Test 2: End rosbag recording failed!")
-            #     pass
-            rospy.loginfo("END of Test2!!!!")
-    
+            print("END of Test2!!!!")
+
     else:
         rospy.loginfo("Else! => Error in code")
-        
-    rospy.spin()
-        
+    print()
+    r.sleep()
