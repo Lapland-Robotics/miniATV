58e5562fe21beb5bef0092144417f0aac69cb770
CStubenvoll <36233290+CStubenvoll@users.noreply.github.com>
2023-09-12 09:47:21 +0300

    Update wintersim_miniatv_start_laptop.sh
    Added delay between starting the WinterSIM and the carla_ros_bridge, so that the world is available for the ros bridge

--- miniATV goes WinterSIM/Start Everything/wintersim_miniatv_start_laptop.sh
+++ miniATV goes WinterSIM/Start Everything/wintersim_miniatv_start_laptop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#export ROS_MASTER_URI=http://cs-worklaptop:11311
 sleep 5s
 # Start everything on the Laptop/PC side:
 # First, make this file executable!
@@ -13,9 +14,8 @@ sleep 5s
 
 
 gnome-terminal --tab -e "WinterSim1.2_rantavitikka/CarlaUE4.sh" 
-#gnome-terminal --tab -e ""
-gnome-terminal --tab -e "roslaunch carla_ros_bridge carla_ros_bridge_with_example_ego_vehicle.launch"
-gnome-terminal --tab -e "roslaunch carla_ackermann_control carla_ackermann_control.launch"
+sleep 5s #Wait for the WinterSIM world to be ready
+gnome-terminal --tab -e "roslaunch carla_ros_bridge gps_carla_ros_bridge_w_ego_vehicle.launch"
 # multimaster_fkie communication between two seperate rosmasters
 # You have to setup the multimaster according to my Multimaster_fkie Documentation.odt or add the setup commands here!
 sleep 6s
@@ -27,4 +27,3 @@ gnome-terminal --tab -e "node_manager"
 gnome-terminal --tab -e "rosrun master_discovery_fkie master_discovery _robot_hosts:=[ATV]"
 sleep 6s
 gnome-terminal --tab -e "rosrun master_sync_fkie master_sync"
-
